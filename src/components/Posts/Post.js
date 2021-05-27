65227b48b28de8ff7bf2d9032700a676050db556
Drewsteroni <drewsteroni@gmail.com>
2021-05-26 19:55:16 -0700

    mvp

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -3,8 +3,8 @@ import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
-const Post = props => {
-  // 🔥 Make sure the parent of Post is passing the right props!
+const Post = (props) => {
+  // :fire: Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
 
   return (
@@ -20,10 +20,10 @@ const Post = props => {
           src={post.imageUrl}
         />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      {/* Is LikeSection getting all the props it needs to work correctly? /}
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
+      {/ Comments also wants its props! */}
+      <Comments comments={post.comments} />
     </div>
   );
 };
